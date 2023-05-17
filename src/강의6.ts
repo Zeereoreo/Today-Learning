2eecf83141da2649df48ecd32031763a3222f50c
Zeereoreo <wltjrcjf12@naver.com>
2023-05-17 15:04:48 +0900

    강의 6,7 업로드

---
+++ src/강의6.ts
@@ -0,0 +1,49 @@
+// 숙제 1
+
+function 클리닝함수(a : (number|string)[]){
+    let array : number[] = []
+    a.forEach((b) => {
+        if (typeof b ==='string') {
+            array.push(parseFloat(b))
+        } else {
+            array.push(b)
+        }
+
+        return array
+    })
+}
+
+console.log(클리닝함수([123,'3']))
+
+// 숙제 2
+
+let 철수쌤 = { subject : 'math' }
+let 영희쌤 = { subject : ['science', 'english'] }
+let 민수쌤 = { subject : ['science', 'art', 'korean'] }
+
+// 지금 여러 변수에 선생님이 가르치고 있는 과목이 저장이 되어있습니다. 
+
+// 과목 1개만 가르치는 쌤들은 문자 하나로 과목이 저장이 되어있고
+
+// 과목 2개 이상 가르치는 쌤들은 array 자료로 과목들이 저장되어있습니다. 
+
+ 
+
+// 철수쌤같은 선생님 object 자료를 집어넣으면 
+
+// 그 선생님이 가르치고 있는 과목중 맨 뒤의 1개를 return 해주는 함수를 만들어봅시다.
+
+// 그리고 타입지정도 엄격하게 해보도록 합시다. 
+
+//ex
+// 만들함수( { subject : 'math' } )  //이 경우 'math'를 return
+// 만들함수( { subject : ['science', 'art', 'korean'] } ) //이 경우 'korean'을 return
+// 만들함수( { hello : 'hi' } )  //이 경우 타입에러 나면 됩니다 
+
+function sub(x:{ subject : string[]}){
+    if(typeof x.subject === 'string'){
+        return x.subject.slice(-1)
+    }else{
+        return 
+    }
+}
---
+++ src/강의7.ts
@@ -0,0 +1,47 @@
+//type alias 만두는 법
+
+type Animal = string | number | undefined  // 변수랑 차이를 주기 위해 대문자 시작 or AnimalType 이렇게 작성
+
+let 동물 : Animal = '원숭이'
+
+type Animals = {name : string, age: number}
+
+let 다른동물:Animals= {name : '원숭이', age : 20}
+
+const 출생지역 = 'seoul' // 바꿀수없는 변수 설정 하기 위해
+출생지역 = 'busan'
+
+const 출생지역2 = {region:'seoul'} // object 자료 수정은 가능함
+출생지역2.region = 'busan'
+
+// object 자료 수정 막기 위한 방법 readonly -> 단지 에디터&터미널에서만 존재, 실제로는 적용됨.
+type Girlfriend = {
+    readonly name : string
+}
+const 여친:Girlfriend = {
+    name : 'ember'
+}
+여친.name = 'ura' // 읽기전용이므로 수정 불가 
+
+//옵션 선택사항 -> ?를 활용 옵션 -> ? = string | undefind
+type Girlfriend = {
+     name? : string
+}
+
+// union type으로 합치기 가능
+type Name = string;
+type Age = number;
+type Person = Nmae | Age;
+
+type PositionX = { x : number };
+type PositionY = { y : number };
+
+{ x : number, y : number } //이 것으로 만들기 위해서는 
+
+type NewType = PositionX & PositionY; // & 로 활용하기
+
+let position :NewType = { x:10, y:20 }
+
+// 주의사항 type 변수 재정의 불가능
+
+type NewType = string // const와 마찬가지
