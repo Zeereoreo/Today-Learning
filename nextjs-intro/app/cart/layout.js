8497868bec269d17217d68c0fec106ba50a5b015
Zeereoreo <wltjrcjf12@naver.com>
2023-06-24 16:37:24 +0900

    next js : 엔드포인트 추가 + 라우팅 안의 layout.js 만들기

---
+++ nextjs-intro/app/cart/layout.js
@@ -0,0 +1,12 @@
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head />
+      <body>
+        <p>현대카드 무이자 이벤트 중</p>
+        {children}
+      </body>
+    </html>
+  )
+} 
