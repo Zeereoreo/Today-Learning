b4bd39e108de0ace713d3c273997d086dc4da762
Zeereoreo <wltjrcjf12@naver.com>
2023-06-26 14:38:50 +0900

    nextjs : img 넣기

--- nextjs-intro/app/list/page.js
+++ nextjs-intro/app/list/page.js
@@ -1,22 +1,14 @@
+
 export default function List() {
   let 상품 = ['totmatoes', 'pasta', 'coconut']
 
   
   return (
       <div>
-        <h2>Products</h2>
-        <div className="food">
-          <h4>{상품[0]} $40</h4>
-        </div>
-        <div className="food">
-          <h4>{상품[1]} $40</h4>
-        </div>
-        <div className="food">
-          <h4>{상품[2]} $40</h4>
-        </div>
         {상품.map((el,idx)=>{
           return(
-          <div className="food">
+          <div className="food" key={idx}>
+            <img src={`/food${idx}.png`} className="food-img"/>
             <h4>{el} $40</h4>
           </div>)
         })}
