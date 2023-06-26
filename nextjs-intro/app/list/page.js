88c33b5d24db6f6372f45b6124823b69ce27f00b
Zeereoreo <wltjrcjf12@naver.com>
2023-06-26 14:23:47 +0900

    nextjs : 상품만들기

--- nextjs-intro/app/list/page.js
+++ nextjs-intro/app/list/page.js
@@ -1,13 +1,25 @@
 export default function List() {
-    return (
+  let 상품 = ['totmatoes', 'pasta', 'coconut']
+
+  
+  return (
       <div>
         <h2>Products</h2>
         <div className="food">
-          <h4>상품명 $40</h4>
+          <h4>{상품[0]} $40</h4>
         </div>
         <div className="food">
-          <h4>상품명 $40</h4>
+          <h4>{상품[1]} $40</h4>
         </div>
+        <div className="food">
+          <h4>{상품[2]} $40</h4>
+        </div>
+        {상품.map((el,idx)=>{
+          return(
+          <div className="food">
+            <h4>{el} $40</h4>
+          </div>)
+        })}
       </div>
     )
   } 
