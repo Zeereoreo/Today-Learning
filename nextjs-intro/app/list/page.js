acbdb2b7ad3eea04fcca6b78474b9012cd58ccd3
Zeereoreo <wltjrcjf12@naver.com>
2023-06-27 18:46:55 +0900

    nextjs : useState

--- nextjs-intro/app/list/page.js
+++ nextjs-intro/app/list/page.js
@@ -1,7 +1,9 @@
+'use client'
+import { useState } from "react"
 
 export default function List() {
   let 상품 = ['totmatoes', 'pasta', 'coconut']
-
+  let [수량, 수량변경]= useState(0)
   
   return (
       <div>
@@ -10,6 +12,8 @@ export default function List() {
           <div className="food" key={idx}>
             <img src={`/food${idx}.png`} className="food-img"/>
             <h4>{el} $40</h4>
+            <span> {수량} </span>
+            <button onClick={()=>{ 수량변경(수량+1) }}> + </button>
           </div>)
         })}
       </div>
