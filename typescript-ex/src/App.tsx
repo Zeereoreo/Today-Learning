1cd0787f7384fb7b3abdc8b82a68979b6d46e361
Zeereoreo <wltjrcjf12@naver.com>
2023-06-28 17:20:38 +0900

    typecript : 강의 20

--- typescript-ex/src/App.tsx
+++ typescript-ex/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4>안녕하세요</h4>
+      <Profile name='철수' age='20'></Profile>
     </div>
   );
+}
+function Profile(props:{name:string, age:string}){
+  return(
+    <div>{props.name}프로필</div>
+  )
 }
 
 export default App;
