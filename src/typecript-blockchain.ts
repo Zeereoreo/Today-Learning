5854b71b5fc2436033bcc25d4e20f1e06b316abb
Zeereoreo <wltjrcjf12@naver.com>
2023-06-30 02:46:01 +0900

    typescript : challenge complete

--- src/typecript-blockchain.ts
+++ src/typecript-blockchain.ts
@@ -10,4 +10,26 @@ function last<T>(arr:T[]){
     return arr[arr.length-1];
 }
 
+function prepend<T>(arr : T[],item : T){
+    return [item,...arr]
+}
 
+function mix<T>(arr1:T[],arr1:T[]){
+    return [...arr1,...arr2]
+}
+
+function count<T>(arr:T[]){
+    return arr.length
+}
+
+function findIndex<T>(arr:T[],item:T){
+    let answer = arr.indexOf(item)
+
+    if(answer)return answer
+    else if (answer === 0) return 0
+    return null
+}
+
+function slice<T>(arr:T[],startIndex:number,endIndex?:number){
+    return arr.slice(startIndex,endIndex)
+}
