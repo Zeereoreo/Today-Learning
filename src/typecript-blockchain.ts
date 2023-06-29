7053d1f53cc352c038e58f6cb47c256be306e84b
Zeereoreo <wltjrcjf12@naver.com>
2023-06-30 02:31:21 +0900

    typescript : challenge

--- src/typecript-blockchain.ts
+++ src/typecript-blockchain.ts
@@ -1,6 +1,13 @@
-type SuperPrint = <T>(a:T[]) => T
 
-const superPrint : SuperPrint = (a) => a[0]
+function superPrint<V>(a:V[]){
+    return a[0]
+}
 
 const a = superPrint([1,2,3,4])
 const b = superPrint([true,false,true])
+
+function last<T>(arr:T[]){
+    return arr[arr.length-1];
+}
+
+
