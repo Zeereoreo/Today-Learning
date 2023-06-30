7b4a4c4630118fdcfd608736095d877a347263ac
Zeereoreo <wltjrcjf12@naver.com>
2023-06-30 15:23:16 +0900

    typecript : redux, redux-toolkit

--- typescript-ex/src/index.tsx
+++ typescript-ex/src/index.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const 초기값 = { count: 0, user : 'kim' };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState : 초기값,
+  reducers: {
+    increment (state){
+      state.count += 1
+    },
+    decrement (state){
+      state.count -= 1
+    },
+    incrementByAmount (state, action :any){
+      state.count += action.payload
+    }
+  }
+})
+
+let store = configureStore({
+  reducer: {
+    counter1 : counterSlice.reducer
+  }
+})
+
+//state 타입을 export 해두는건데 나중에 쓸 데가 있음
+export type RootState = ReturnType<typeof store.getState>
+
+//수정방법 만든거 export
+export let {increment, decrement, incrementByAmount} = counterSlice.actions
