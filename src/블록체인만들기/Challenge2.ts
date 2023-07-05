3875509df40ae5c674dae127758d965e98ee3413
Zeereoreo <wltjrcjf12@naver.com>
2023-07-05 19:10:37 +0900

    블록체인 첼린지

--- src/블록체인만들기/Challenge2.ts
+++ src/블록체인만들기/Challenge2.ts
@@ -45,3 +45,88 @@ geolocation.watchPosition(success, error, options);
 geolocation.clearWatch(id);
 
 // Documentation: https://developer.mozilla.org/en-US/docs/Web/API/Geolocation
+
+interface IGeolocation {
+    clearWatch(watchId: number): void;
+    getCurrentPosition(
+      successCallback: IPositionCallback,
+      errorCallback?: IPositionErrorCallback | null,
+      options?: IGeolocationOptions
+    ): void;
+    watchPosition(
+      successCallback: PositionCallback,
+      errorCallback?: IPositionErrorCallback | null,
+      options?: IGeolocationOptions
+    ): number;
+  }
+  
+  // successCallback interface
+  interface IPositionCallback {
+    (position: IGeolocationPosition): void;
+  }
+  interface IGeolocationPosition {
+    readonly coords: IGeolocationCoordinates;
+    readonly timestamp: IEpochTimeStamp;
+  }
+  interface IGeolocationCoordinates {
+    readonly accuracy: number;
+    readonly altitude: number | null;
+    readonly altitudeAccuracy: number | null;
+    readonly heading: number | null;
+    readonly latitude: number;
+    readonly longitude: number;
+    readonly speed: number | null;
+  }
+  type IEpochTimeStamp = number;
+  
+  // errorCallback interface
+  interface IPositionErrorCallback {
+    (positionError: IGeolocationPositionError): void;
+  }
+  interface IGeolocationPositionError {
+    readonly code: number;
+    readonly message: string;
+    readonly PERMISSION_DENIED: number;
+    readonly POSITION_UNAVAILABLE: number;
+    readonly TIMEOUT: number;
+  }
+  
+  // option interface
+  interface IGeolocationOptions {
+    enableHighAccuracy: boolean;
+    timeout: number;
+    maximumAge: number;
+  }
+  
+  // Geolocation class
+  class GGeolocation implements IGeolocation {
+    clearWatch(watchId: number) {
+      console.log(watchId);
+    }
+    getCurrentPosition(
+      successCallback: IPositionCallback,
+      errorCallback?: PositionErrorCallback | null,
+      options?: PositionOptions
+    ) {
+      if (successCallback) console.log(successCallback);
+      if (errorCallback) console.log(errorCallback);
+      if (options) console.log(options);
+    }
+    watchPosition(
+      successCallback: PositionCallback,
+      errorCallback?: PositionErrorCallback | null,
+      options?: PositionOptions
+    ) {
+      if (successCallback) console.log(successCallback);
+      if (errorCallback) console.log(errorCallback);
+      if (options) console.log(options);
+      return 0;
+    }
+  }
+  
+  const geolocation = new GGeolocation();
+  geolocation.getCurrentPosition(test);
+  geolocation.getCurrentPosition(test, test);
+  geolocation.getCurrentPosition(test, test, {});
+  
+  function test() {}
