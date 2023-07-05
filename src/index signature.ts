ae932d24fd8fbd922605886199d71f490d07f266
Zeereoreo <wltjrcjf12@naver.com>
2023-07-05 19:03:11 +0900

    typescript : index signatuer 숙제

--- src/index signature.ts
+++ src/index signature.ts
@@ -70,3 +70,38 @@ interface StringOnly {
       }
     }
   }
+
+//   (숙제1) 다음 자료의 타입을 지정해보십시오. 
+
+
+interface Type {
+    [key :string] : string | number
+}
+
+  let obj1 :Type = {
+    model : 'k5',
+    brand : 'kia',
+    price : 6000,
+    year : 2030,
+    date : '6월',
+    percent : '5%',
+    dealer : '김차장',
+  }
+
+//   (숙제2) 다음 object 자료의 타입을 interface 써서 만들어보십시오. 
+
+interface Type0 {
+    'font-size' : number,
+  [key :string] : number | MyType,
+}
+
+
+let obj3 : Type0= {
+    'font-size' : 10,
+    'secondary' : {
+      'font-size' : 12,
+      'third' : {
+        'font-size' : 14
+      }
+    }
+  }
