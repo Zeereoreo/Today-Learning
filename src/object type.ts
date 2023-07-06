3d1cb05ceb42c7d9bccc12364532692726d077c5
Zeereoreo <wltjrcjf12@naver.com>
2023-07-06 23:25:56 +0900

    typescript : object type 변환 숙제2

--- src/object type.ts
+++ src/object type.ts
@@ -91,3 +91,34 @@ type TypeChanger <MyType> = {
   };
   
   type NewBus = TypeChanger<Bus>
+
+//   (숙제2) 이런 변환기는 어떻게 만들어야할까요?
+
+object안에 들어있는 모든 속성을
+
+string, number 이렇게 고정된 타입으로 변환해주는게 아니라
+
+내가 원하는 타입을 입력하면 그걸로 변환해주는 범용성 좋은 변환기를 만들어보십시오.
+
+type Bus = {
+    color : string,
+    model : boolean,
+    price : number
+  }
+  
+  type TypeChanger <MyType, T> = {
+    [key in keyof MyType]: T;
+  };
+  
+  type NewBus = TypeChanger<Bus, boolean>;
+  type NewBus2 = TypeChanger<Bus, string[]>
+
+  이러면 TypeChanger 쓸 때마다 타입파라미터를 T 자리에 하나 더 입력할 수 있게 됩니다.
+
+그러면 이제 오브젝트 모든 속성은 T로 바뀜
+
+ 
+
+NewBus 살펴보시면 모든 속성이 boolean으로 바뀌어있습니다.
+
+NewBus2 살펴보시면 모든 속성이 string[] 으로 바뀌어있습니다.
